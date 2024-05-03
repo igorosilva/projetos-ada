b212cd97f12c8df8f8d6dce021b5de912dedeeda
igorosilva <igoroliveirasilva772@gmail.com>
2024-05-03 17:32:39 -0300

    Fixing University Project

--- Banco de dados/universidade.sql
+++ Banco de dados/universidade.sql
@@ -10,15 +10,20 @@ create table UNIDADE_ACADEMICA(
 	nome varchar(50) not null
 );
 
-create table PROFESSOR(
+create table CONTRATADO(
 	matricula serial primary key,
 	codigo_unidade_academica serial,
 	nome varchar(50) not null,
 	email varchar(100) not null,
 	salario decimal(10,2) not null check(salario >= 0),
-	formacao varchar(50) not null,
 	data_inicio date not null default current_date,
 	foreign key(codigo_unidade_academica) references UNIDADE_ACADEMICA(codigo)
+);
+
+create table PROFESSOR(
+	matricula serial primary key,
+	formacao varchar(50) not null,
+	foreign key(matricula) references CONTRATADO(matricula) on delete cascade
 );
 
 create table TURMA(
@@ -44,83 +49,73 @@ create table ALUNO_TURMA(
 
 create table FUNCIONARIO(
 	matricula serial primary key,
-	codigo_unidade_academica serial,
-	nome varchar(50) not null,
-	email varchar(100) not null,
-	salario decimal(10,2) not null check(salario >= 0),
 	funcao varchar(50) not null,
-	data_inicio date not null default current_date,
-	foreign key(codigo_unidade_academica) references UNIDADE_ACADEMICA(codigo)
+	foreign key(matricula) references CONTRATADO(matricula) on delete cascade
 );
 
 create table DEPENDENTE(
 	id serial primary key,
 	matricula_contratado serial,
-	tipo_contratado varchar(11) not null default 'Funcionario',
 	nome varchar(50) not null,
 	data_nascimento date not null default current_date,
-	foreign key(matricula_contratado) references professor(matricula) on delete cascade,
-	foreign key(matricula_contratado) references funcionario(matricula) on delete cascade,
-	unique(matricula_contratado, tipo_contratado, nome, data_nascimento)
+	cpf varchar(14) not null,
+	foreign key(matricula_contratado) references CONTRATADO(matricula) on delete cascade,
+	unique(cpf)
 );
 
 -- Views
 create view DETALHES_AULA as
 	select at.aluno_matricula as matricula_aluno, 
 		a.nome as "Aluno",
-		p.nome as "Professor",
+		c.nome as "Professor",
 		t.nome_disciplina as "Disciplina", 
 		t.horario as "Horario"
 	from ALUNO_TURMA at
 		inner join ALUNO a on at.aluno_matricula = a.matricula
 		inner join TURMA t on at.turma_numero = t.numero
-		left join PROFESSOR p on t.matricula_professor = p.matricula;
+		left join PROFESSOR p on t.matricula_professor = p.matricula
+        left join CONTRATADO c on p.matricula = c.matricula;
 
 create view CONTRATADOS as
-	select p.matricula as matricula, 
+	select c.matricula as matricula, 
 		'Professor' as tipo_contratado,
-		p.nome as "Nome", 
+		c.nome as "Nome", 
 		p.formacao as formacao_professor,
+		c.salario as salario,
 		ua.codigo as codigo_unidade,
 		ua.nome as nome_unidade
-	from PROFESSOR p
-	inner join UNIDADE_ACADEMICA ua on p.codigo_unidade_academica = ua.codigo
+	from CONTRATADO c
+		inner join UNIDADE_ACADEMICA ua on c.codigo_unidade_academica = ua.codigo
+		inner join PROFESSOR p on c.matricula = p.matricula
 	union all
 	select f.matricula as matricula,
 		'Funcionario' as tipo_contratado,
-		f.nome as "Nome",
-		f.funcao,
+		c.nome as "Nome",
+		f.funcao as funcao_funcionario,
+		c.salario as salario,
 		ua.codigo as codigo_unidade,
 		ua.nome as nome_unidade
-	from FUNCIONARIO f
-	inner join UNIDADE_ACADEMICA ua on f.codigo_unidade_academica = ua.codigo;
+	from CONTRATADO c
+		inner join UNIDADE_ACADEMICA ua on c.codigo_unidade_academica = ua.codigo
+		inner join FUNCIONARIO f on c.matricula = f.matricula;
 
 create view DETALHES_DEPENDENTES as
-	select p.matricula as matricula,
-		p.nome as "Contratado",
-		d.tipo_contratado as "Tipo Contratado",
+	select c.matricula as matricula,
+		c.nome as "Contratado",
 		d.nome as "Dependente",
+		d.cpf as "CPF do Dependente",
 		d.data_nascimento as "Data Nascimento Dependente"
-	from PROFESSOR p
-	inner join DEPENDENTE d on p.matricula = d.matricula_contratado
-	where d.tipo_contratado = 'Professor'
-	union all
-	select f.matricula as matricula,
-		f.nome as "Contratado",
-		d.tipo_contratado as "Tipo Contratado",
-		d.nome as "Dependente",
-		d.data_nascimento as "Data Nascimento Dependente"
-	from FUNCIONARIO f
-	inner join DEPENDENTE d on f.matricula = d.matricula_contratado
-	where d.tipo_contratado = 'Funcionario';
+	from CONTRATADO c
+		inner join DEPENDENTE d on c.matricula = d.matricula_contratado;
+
 
 -- Inserts
 insert into ALUNO 
 (matricula, nome, email, mensalidade) 
 values
-    (1, 'João Silva', 'joao@example.com', 500.00),
-    (2, 'Maria Santos', 'maria@example.com', 550.00),
-    (3, 'Carlos Oliveira', 'carlos@example.com', 600.00);
+    (1, 'Wesley Silva', 'wesley@email.com', 500.00),
+    (2, 'Samatha Santos', 'samantha@email.com', 550.00),
+    (3, 'Roberto Oliveira', 'roberto@email.com', 600.00);
 
 insert into UNIDADE_ACADEMICA 
 (codigo, nome) 
@@ -128,13 +123,23 @@ values
     (1, 'Departamento de Informática'),
     (2, 'Departamento de Matemática'),
     (3, 'Departamento de Ciências Sociais');
+   
+insert into CONTRATADO 
+(matricula, codigo_unidade_academica, nome, email, salario, data_inicio) 
+values
+    (1, 1, 'Gabriel Pereira', 'gabriel@email.com', 3500.00, '2022-05-09'),
+    (2, 2, 'Matheus Santos', 'matheus@email.com', 3800.00, '2023-04-11'),
+    (3, 3, 'Mariana Lima', 'mariana@email.com', 3200.00, '2024-01-01'),
+    (4, 1, 'Ana Oliveira', 'ana@email.com', 2800.00, '2021-05-02'),
+    (5, 2, 'Carlos Santos', 'carlos@email.com', 3200.00, '2022-07-01'),
+    (6, 3, 'Leonardo Silva', 'leonardo@email.com', 3000.00, '2020-08-08');
 
 insert into PROFESSOR 
-(matricula, nome, email, salario, formacao, codigo_unidade_academica, data_inicio) 
+(matricula, formacao) 
 values
-    (1, 'Ana Pereira', 'ana@example.com', 3500.00, 'Doutorado em Informática', 3, '2022-05-09'),
-    (2, 'Pedro Santos', 'pedro@example.com', 3800.00, 'Mestrado em Matemática', 2, '2023-04-11'),
-    (3, 'Mariana Lima', 'mariana@example.com', 3200.00, 'Doutorado em Sociologia', 1, '2024-01-01');
+    (1, 'Doutorado em Informática'),
+    (2, 'Mestrado em Matemática'),
+    (3, 'Doutorado em Sociologia');
 
 insert into TURMA 
 (numero, codcred, nome_disciplina, horario, matricula_professor) 
@@ -151,25 +156,20 @@ values
     (3, 3, 3, 103, 1);
 
 insert into FUNCIONARIO 
-(matricula, nome, email, salario, funcao, codigo_unidade_academica, data_inicio) 
+(matricula, funcao) 
 values
-    (1, 'José Silva', 'jose@example.com', 3000.00, 'Secretário', 1, '2020-08-08'),
-    (2, 'Ana Oliveira', 'anao@example.com', 2800.00, 'Assistente Administrativo', 2, '2021-05-02'),
-    (3, 'Carlos Santos', 'carloss@example.com', 3200.00, 'Auxiliar de Limpeza', 3, '2022-07-01');
+    (4, 'Bibliotecário'),
+    (5, 'Assistente Administrativo'),
+    (6, 'Auxiliar de Limpeza');
 
 insert into DEPENDENTE 
-(id, matricula_contratado, tipo_contratado, nome, data_nascimento) 
+(id, matricula_contratado, nome, data_nascimento, cpf) 
 values
-    (1, 1, 'Professor', 'Pedro Silva', '2010-05-15'),
-    (2, 3, 'Professor', 'Mariana Oliveira', '2012-08-20'),
-    (3, 2, 'Professor', 'João Santos', '2008-03-10');
-
-insert into DEPENDENTE 
-(id, matricula_contratado, tipo_contratado, nome, data_nascimento) 
-values
-    (4, 1, 'Funcionario', 'Maria Silva', '2005-12-10'),
-    (5, 2, 'Funcionario', 'Pedro Oliveira', '2011-06-25'),
-    (6, 3, 'Funcionario', 'Ana Santos', '2009-09-03');
+    (1, 1, 'Mauricio Silva', '2010-05-15', '123.456.789-44'),
+    (2, 3, 'Giovanna Oliveira', '2012-08-20', '987.654.321-00'),
+    (3, 2, 'João Santos', '2008-03-10', '111.222.333-44'),
+    (4, 1, 'Maria Silva', '2005-12-10', '555.666.777-88'),
+    (5, 2, 'Pedro Oliveira', '2011-06-25', '999.000.123-45');
 
 -- Selects
 select * from ALUNO;
@@ -179,10 +179,17 @@ select * from PROFESSOR;
 select * from DETALHES_AULA;
 select * from UNIDADE_ACADEMICA;
 select * from FUNCIONARIO;
-select * from DEPENDENTE order by id;
-select * from CONTRATADOS order by matricula;
+
+select * from DEPENDENTE order by matricula_contratado;
+
+select * from CONTRATADOS;
 select * from CONTRATADOS where tipo_contratado='Professor';
 select * from CONTRATADOS where tipo_contratado='Funcionario';
+
 select * from DETALHES_DEPENDENTES order by matricula;
-select * from DETALHES_DEPENDENTES where "Tipo Contratado"='Professor';
-select * from DETALHES_DEPENDENTES where "Tipo Contratado"='Funcionario';
+
+select avg(salario) as "Media Salarial" from CONTRATADOS;
+select min(salario) as "Menor Salário" from CONTRATADOS;
+select max(salario) as "Maior Salário" from CONTRATADOS;
+select sum(salario) as "Soma de Todos os Salários" from CONTRATADOS;
+select count(*) as "Qtd. Total de Funcionarios" from CONTRATADOS;
